706b574230e6bc33536891201974125896121e4c
liangyuyi <liangyuyi@bytedance.com>
2018-04-24 22:17:57 +0800

    modify sso api server

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_API: '"http://52.78.119.221:801/api/"',
+    BASE_API: '"http://sso.wuanla.tk/api/"',
     APP_ORIGIN: '"http://wuanlife.com"',
     APP_OIDC_REDIRECT: '"/cb"',  // OIDC重定向地址
     // 七牛域名
