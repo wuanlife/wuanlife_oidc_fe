f453e091683eb3e69618633b226f08a9bfb4efe4
icelo0 <36466638+icelo0@users.noreply.github.com>
2018-05-30 22:12:37 +0800

    修改密码页面修改 (#6)
    * 顶部导航加判断
    
    * 增加找回密码和修改密码  接口未调通
    
    * 修改密码页面个人资料导航修正
    
    * 找回密码页面
    
    * 修改密码页面修改

--- src/views/personal/points/index.vue
+++ src/views/personal/points/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>wori</div>
+  <div class="view-container login-container">
+    <div class="form-content">
+    <span>您当前拥有</span>
+    <span>影视积分</span>
+    <span><i class="el-icon-date"></i>54</span>
+    <span>午安币</span>
+    <span><i class="el-icon-date"></i>16</span>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -17,5 +25,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.login-container {
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
+  width: 700px;
+  height: 343px;
+  .form-content {
+    /*background: #C0C0C0;*/
+    background: #ffffff;
+    width: 400px;
+    height: 320px;
+    margin: 0 auto;
+    justify-content: center;
+    box-shadow: 0px 0px 2px 2px #ecedee;
+    text-align: center;
+    border-radius: 5px;
+  }
+}
 </style>
