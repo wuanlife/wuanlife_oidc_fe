4ec547233cb03080cc5b18680ae767dcf67a6a70
sssby <527332935@qq.com>
2018-05-30 13:06:21 +0800

    profile commit

---
+++ src/components/DatePicker/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="date-picker" @click="open" :class="{ 'date-picker-clicked': isClick }">
+      <span>{{ defaultNum }}<i class="el-icon-caret-bottom"></i></span>
+      <ul v-if="isClick">
+          <li v-for="n in (max - min + 1)" :key="n" @click="picker(n)">{{ (min + n - 1)}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'datePicker',
+  props: {
+    min: {
+      type: Number,
+      required: true
+    },
+    max: {
+      type: Number,
+      required: true
+    },
+    defaultNum: {
+      type: Number,
+      required: true
+    }
+  },
+  data () {
+    return {
+      isClick: false
+    }
+  },
+  methods: {
+    picker: function (value) {
+      this.defaultNum = this.min + value - 1
+      this.$emit('pick', this.defaultNum)
+    },
+    open: function () {
+      this.isClick = !this.isClick
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.date-picker{
+    max-width: 80px;
+    text-align: center;
+    box-shadow: 0px 3px 7px 0px
+    rgba(99, 99, 99, 0.16);
+  border: solid 2px rgba(171, 171, 171, 0.45);
+    background-color: #f1f1f1;
+  border-radius: 4px;
+    padding: 14px 6px 13px 6px;
+    cursor: pointer;
+    position: relative;
+    span{
+        font-size: 17px;
+        color: #757575;
+    }
+    ul{
+        height: 174px;
+        width: 100%;
+      background-color: #f8f9fa;
+      box-shadow: 0px 3px 7px 0px
+    rgba(86, 119, 252, 0.4);
+      border-radius: 4px;
+      border: solid 2px #99b3e3;
+        font-size: 16px;
+        color: #5677fc;
+        padding: 10px 0 10px 0;
+        overflow-y: scroll;
+        cursor: pointer;
+        position: absolute;
+        top: 48px;
+        left: 0;
+        li{
+            margin-bottom: 5px;
+            padding: 5px 0 5px 0x;
+            &:hover{
+                color: white;
+                background-color: #5677fc;
+            }
+        }
+    }
+}
+.date-picker-clicked{
+    background-color: rgba(248, 249, 250, 0.4);
+    box-shadow: 0px 3px 7px 0px
+    rgba(86, 119, 252, 0.16);
+    border: solid 2px rgba(0, 64, 185, 0.4);
+}
+.avatar-icon{
+    margin-left: 5px;
+    color: #5677fc;
+}
+</style>
--- src/views/personal/profile/index.vue
+++ src/views/personal/profile/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="personal-data" class="personal-data view-container wl-card">
     <section>
-      <h1>个人资料</h1>
       <div class="personal-data-form" v-loading="loading1">
           <div class="form-left" v-loading="loading">
               <img v-bind:src="dafaultAvatarUrl" id="avatar" ref="avatar">
@@ -17,7 +16,48 @@
                  size="small"
                  type="primary">点击上传</el-button>
                 </el-upload>
-              <button @click="changeAvatar"><icon-svg icon-class="modify" class="avatar-icon"></icon-svg>修改</button>
+              <button><i class="el-icon-edit-outline avatar-icon "></i>修改</button>
+          </div>
+          <div class="form-right">
+              <div class="form-item">
+                  <span>邮箱:</span>
+                  <p>123@qq.com</p>
+              </div>
+              <div class="form-item">
+                  <span>昵称:</span>
+                  <input type="text" v-model="name">
+              </div>
+              <div class="form-item">
+                  <span>性别:</span>
+                  <div class="form-item-sex">
+                      <el-radio  v-model="sex" label="male">男</el-radio>
+                      <el-radio  v-model="sex" label="female">女</el-radio>
+                      <el-radio  v-model="sex" label="secrecy">保密</el-radio>
+                  </div>
+              </div>
+              <div class="form-item">
+                  <span>生日:</span>
+                  <div class="form-item-date">
+                      <date-picker
+                        :min="1970"
+                        :max="2018"
+                        :defaultNum="yearNumber"
+                        @pick="year"
+                        class="date-picker"></date-picker>年
+                      <date-picker
+                        :min="1"
+                        :max="12"
+                        :defaultNum="mouthNumber"
+                        @pick="mouth"
+                        class="date-picker"></date-picker>月
+                      <date-picker
+                        :min="1"
+                        :max="dayMax"
+                        :defaultNum="dayNumber"
+                        @pick="day"
+                        class="date-picker"></date-picker>日
+                  </div>
+              </div>
           </div>
         </div>
     </section>
@@ -29,17 +69,17 @@
 // import { Notification } from 'element-ui'
 
 // const QINIU_DOMAIN = '//7xlx4u.com1.z0.glb.clouddn.com/' // 图片服务器域名，展示时用
-
+import DatePicker from 'components/DatePicker'
 export default {
   name: 'personalData',
   data () {
     return {
-      yearNumber: 1970,
+      yearNumber: 2018,
       mouthNumber: 1,
       dayNumber: 1,
       dayMax: 31,
       leap: false,
-      sex: '',
+      sex: 'female',
       mail: '',
       name: '',
       dafaultAvatarUrl: 'http://7xlx4u.com1.z0.glb.clouddn.com/o_1aqt96pink2kvkhj13111r15tr7.jpg?imageView2/1/w/97/h/97',
@@ -51,35 +91,67 @@ export default {
       default: {}
     }
   },
+  components: {
+    DatePicker
+  },
   computed: {
+  },
+  methods: {
+    year: function (val) {
+      this.yearNumber = val
+      // 判断闰年
+      let isLeap = (this.yearNumber % 4 === 0 && this.yearNumber % 100 !== 0) || this.yearNumber % 400 === 0
+      if (isLeap) {
+        this.leap = true
+      } else {
+        this.leap = false
+      }
+    },
+    mouth: function (val) {
+      this.mouthNumber = val
+      // 相应改变每月的天数
+      if (this.mouthNumber === 1 || this.mouthNumber === 3 || this.mouthNumber === 5 || this.mouthNumber === 7 || this.mouthNumber === 8 || this.mouthNumber === 10 || this.mouthNumber === 12) {
+        this.dayMax = 31
+      } else {
+        this.dayMax = 30
+      }
+      if (this.mouthNumber === 2) {
+        if (this.leap === true) {
+          this.dayMax = 29
+        } else {
+          this.dayMax = 28
+        }
+      }
+    },
+    day: function (val) {
+      this.dayNumber = val
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style rel="stylesheet/scss" lang="scss" scoped>
 #personal-data{
-    width: 720px;
-    margin-top: 119px;
-    margin-bottom: 36px;
-    background-color: blue;
+    width: 465px;
+    margin: 0 auto;
     border-radius: 4px;
-    padding: 42px 111px 68px 121px;
+    padding: 0px 111px 68px 121px;
     text-align: center;
     section{
-        h1{
-            font-size: 20px;
-            color: #5677fc;
-            margin-bottom: 42px;
-        }
         .personal-data-form{
             display: flex;
             justify-content: space-between;
             .form-left{
-              background-color: #434343;
+              padding-top:45px;
+              margin-right: 30px;
+                .el-icon-edit-outline::before{
+                  height:45px;
+                  width:45px;
+                }
                 img{
                     display: block;
-                    height: 97px;
-                    width: 97px;
+                    height: 100px;
+                    width: 100px;
                     border-radius: 100%;
                     background-color: rgb(165, 164, 164);
                     box-shadow: 0px 4px 5px 0px rgba(181, 181, 181, 0.75);
@@ -97,69 +169,41 @@ export default {
                 }
             }
             .form-right{
-                border-left: solid 2px #c9c9c9;
+                border-left: solid 1px #c9c9c9;
+                margin-top: 45px;
                 padding-left: 26px;
                 .form-item{
+                    min-height: 35px;
                     display: flex;
-                    min-height: 45px;
                     align-items: center;
-                    font-size: 17px;
-                    color: #434343;
-                    margin-bottom: 37px;
+                    color: #707070;
+                    margin-bottom: 20px;
                     &:last-child{
                         margin-bottom: 0;
                     }
                     span{
-                        margin-right: 32px;
-                        font-size: 15px;
-                        color: #434343;
+                        padding-right:18px;
+                        font-size: 14px;
+                        font-weight: bold;
+                        color: #707070;
                     }
                     &>input{
-                        height: 45px;
-                        width: 253px;
-                        color: #434343;
-                        box-shadow: 0px 3px 7px 0px
-                    rgba(99, 99, 99, 0.16);
-                      border-radius: 4px;
-                      border: solid 2px rgba(171, 171, 171, 0.45);
-                        padding: 0 16px;
+                        height: 28px;
+                        width: 250px;
+                        background-color: #f1f1f1;
+                        border-radius: 3px;
+                        padding-left: 8px;
                     }
                     &>input:focus{
                         background-color: rgba(248, 249, 250, 0.4);
-                      box-shadow: 0px 3px 7px 0px
-                    rgba(86, 119, 252, 0.16);
-                      border-radius: 4px;
-                      border: solid 2px rgba(0, 64, 185, 0.4);
+                        border-radius: 4px;
+                         border: solid 1px rgba(0, 64, 185, 0.4);
                     }
                     .form-item-sex{
                         display: flex;
                         align-items: center;
                         justify-content: flex-start;
                         width: 253px;
-                        input{
-                            width: 0;
-                            height: 0;
-                        }
-                        label{
-                            width: 65px;
-                            height: 44px;
-                            line-height: 44px;
-                            display: inline-block;
-                            cursor: pointer;
-                            background-color: rgba(248, 249, 250, 0.45);
-                          box-shadow: 0px 3px 7px 0px
-                        rgba(99, 99, 99, 0.16);
-                          border-radius: 4px;
-                          border: solid 2px rgba(171, 171, 171, 0.45);
-                            font-size: 17px;
-                            color: #434343;
-                            font-weight: normal;
-                            margin-right: 29px;
-                            &:last-child{
-                                font-size: 13px;
-                                margin-right: 0;
-                            }
-                        }
                         .label-active{
                             background-color: rgba(248, 249, 250, 0.4);
                           box-shadow: 0px 3px 7px 0px
